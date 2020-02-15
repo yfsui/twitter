4315d3ff7683cc96bcc386851afabd44937a75fc
SYF <anniesui9627@gmail.com>
2020-02-15 13:06:41 -0500

    add test version 2

--- twitter_test.py
+++ twitter_test.py
@@ -1,28 +1,50 @@
-import unittest  # library for unit testing
+"""
+test of twitter.py
+"""
+import unittest  
 import twitter as TwitterClass
 
 class TestMyModule(unittest.TestCase):
-    text = 'RT @mytsvideo: Gia Itzel.\n\n100% free shemale cams. Enter now:  https://t.co/qJqq3VXTxg\n\n#shemale #TS #tgirl https://t.co/GMBmcJkz1E'
-    twitter = TwitterClass.Twitter(text)
+    """
+    test of class Twitter
+    """
+    text = """RT @mytsvideo: Gia Itzel.\n\n100% free shemale cams.
+    Enter now:  https://t.co/qJqq3VXTxg\n\n#shemale #TS #tgirl https://t.co/GMBmcJkz1E"""
+    twitter = TwitterClass.Twitter(text, max_length_dictionary=100000, max_length_tweet=10)
 
     def test_clean_text(self):
+        """
+        test of clean_text
+        """
         result = self.twitter.clean_text()
-        expected_result = 'Gia Itzel   100  free shemale cams  Enter now     TS  tgirl '
-        self.assertEqual(result,expected_result)
+        expected_result = 'Gia Itzel   100  free shemale cams      Enter now     TS  tgirl '
+        self.assertEqual(result, expected_result)
 
     def test_tokenize_text(self):
+        """
+        test of tokenize_text
+        """
         result = self.twitter.tokenize_text()
-        expected_result = ['Gia', 'Itzel','100','free','shemale','cams','Enter','now','TS','tgirl']
-        self.assertEqual(result,expected_result)
+        expected_result = ['Gia', 'Itzel', '100', 'free', 'shemale', 'cams',
+                           'Enter', 'now', 'TS', 'tgirl']
+        self.assertEqual(result, expected_result)
 
     def test_replace_token_with_index(self):
-        self.twitter.tokens = ['Gia','Itzel','100','free','shemale','cams','Enter','now','TS','tgirl']
+        """
+        test of replace_token_with_index
+        """
+        self.twitter.tokens = self.twitter.tokenize_text()
         result = self.twitter.replace_token_with_index()
-        expected_result = [24477, 122718, 338, 40136, 32649, 2289, 71, 3931, 176800]
-        self.assertEqual(result,expected_result)
+        expected_result = [24477, 338, 40136, 32649, 2289, 71, 3931]
+        self.assertEqual(result, expected_result)
 
     def test_pad_sequence(self):
-        self.twitter.indexes = [24477, 122718, 338, 40136, 32649, 2289, 71, 3931, 176800]
+        """
+        test of pad_sequence
+        """
+        self.twitter.tokens = self.twitter.tokenize_text()
+        self.twitter.indexes = self.twitter.replace_token_with_index()
         result = self.twitter.pad_sequence()
-        expected_result = [24477, 122718, 338, 40136, 32649, 2289, 71, 3931, 176800]
-        self.assertEqual(result,expected_result)
+        expected_result = [24477, 338, 40136, 32649, 2289, 71, 3931, 0, 0, 0]
+        self.assertEqual(result, expected_result)
+        
